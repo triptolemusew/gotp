38a9b88bfa56663dbd3cf1e21a7255470d68cdc8
faris <deneuve.crepes@gmail.com>
2024-02-09 09:20:46 +0100

    Using simpler cli

--- main.go
+++ main.go
@@ -1,7 +1,98 @@
 package main
 
-import "github.com/triptolemusew/gotp/cmd"
+import (
+	"log"
+	"os"
+	"time"
+
+	ui "github.com/gizak/termui/v3"
+	"github.com/triptolemusew/gotp/cmd"
+	"github.com/triptolemusew/gotp/db"
+	"github.com/triptolemusew/gotp/tui"
+	"github.com/urfave/cli/v2"
+)
+
+const VERSION = "0.1"
 
 func main() {
-	cmd.Execute()
+	app := &cli.App{
+		Name:    "Gotp",
+		Usage:   "Get your token from cli",
+		Version: VERSION,
+		Commands: []*cli.Command{
+			{
+				Name:    "add",
+				Aliases: []string{"a"},
+				Usage:   "Add account",
+				Action: func(ctx *cli.Context) error {
+					if err := ui.Init(); err != nil {
+						log.Fatalf("failed to initialize termui: %v", err)
+					}
+					defer ui.Close()
+
+					tuiManager := tui.NewManager()
+					tuiManager.InitializeWidgets()
+
+					startApp(tuiManager)
+
+					return nil
+				},
+			},
+			{
+				Name:    "remove",
+				Aliases: []string{"r"},
+				Usage:   "Remove account",
+				Action: func(ctx *cli.Context) error {
+					return nil
+				},
+			},
+			{
+				Name:    "sync",
+				Aliases: []string{"s"},
+				Usage:   "Sync accounts with Gotp folder",
+				Action: func(ctx *cli.Context) error {
+					err := cmd.SyncCommandExecution(ctx)
+					return err
+				},
+			},
+			{
+				Name:    "init",
+				Aliases: []string{"i"},
+				Usage:   "Initialize the application",
+				Action: func(ctx *cli.Context) error {
+					if err := db.Migrate(".gotp"); err != nil {
+						return err
+					}
+					return nil
+				},
+			},
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func startApp(tuiManager *tui.Manager) {
+	uiRefreshTicker := time.NewTicker(1 * time.Millisecond)
+	defer uiRefreshTicker.Stop()
+
+	uiEvents := ui.PollEvents()
+
+	for {
+		select {
+		case e := <-uiEvents:
+			switch e.ID {
+			case "q", "<C-c>":
+				return
+			case "<Backspace>":
+				tuiManager.RemoveLastCharBuffer()
+			default:
+				tuiManager.HandleBuffer(e.ID)
+			}
+		case <-uiRefreshTicker.C:
+			tuiManager.UpdateWidgets(1, 1)
+		}
+	}
 }
